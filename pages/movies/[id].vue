31c7f6460246a6dfafaf3b44756496fa8d5a1ab9
XefDavid <david.garcia@cesefor.com>
2024-12-22 19:04:50 +0100

    mapping {id].vue

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -12,8 +12,17 @@ const { data } = useAsyncData(() => {
 </script>
 
 <template>
-	<div>
-		<h1>{{ data }}</h1>
+	<nuxt-link to="/">
+		<button class="bg-blue-300 rounded-xl w-20 h-8">Back</button>
+	</nuxt-link>
+	<div class="flex flex-col w-300px h-400px">
+		<h1>{{ data.Title }}</h1>
+		<img :src="data.Poster" alt="movie" />
+		<p>{{ data.Year }}</p>
+		<p>{{ data.Rated }}</p>
+		<p>{{ data.Runtime }}</p>
+		<p>{{ data.Released }}</p>
+		<p>{{ data.Genre }}</p>
 	</div>
 	<pre>{{ data }}</pre>
 </template>
