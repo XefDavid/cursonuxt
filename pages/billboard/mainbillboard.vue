1ddb69d0584c48f642907d88e3b63dfd676d84f9
David <156122937+XefDavid@users.noreply.github.com>
2025-02-23 09:27:42 +0100

    style: changed backgrouind style

--- pages/billboard/mainbillboard.vue
+++ pages/billboard/mainbillboard.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-import Navbar from "~/components/navbar.vue";
 import Billboard from "~/components/Billboard/Billboard.vue";
+import Navbar from "~/components/navbar.vue";
 </script>
 
 <template>
-	<div
-		id="imageBackground"
-		class="aspect-[16/9] w-screen bg-cover bg-center border-[5px] border-blue-500 gap-5"
-		:style="{ backgroundImage: `url('/images/background.webp')` }"
-	>
+	<div id="imageBackground" class="aspect-[16/9] w-screen bg-cover bg-center border-[5px] border-blue-500 gap-5"
+		:style="{ backgroundImage: `url('/images/background.webp')` }">
 		<Navbar />
 		<Billboard />
 	</div>
