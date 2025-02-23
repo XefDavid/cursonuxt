1c6e31dc497b43ac9f8e2614f2ecc12f71754b8b
David <156122937+XefDavid@users.noreply.github.com>
2025-02-23 09:26:25 +0100

    add: nuxtlink to edit profile

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
+import { useRouter } from "vue-router";
 import { useGeoLocation } from "~/composables/useGeoLocation";
 import { useWeather } from "~/composables/useWeather";
-import { useRouter } from "vue-router";
 
 const router = useRouter();
 
@@ -31,24 +31,14 @@ const handleLogout = () => {
 
 <template>
 	<div class="text-blue-900">
-		<div
-			class="flex flex-row items-center text-sm h-14 w-full bg-blue-300 p-5 opacity-90"
-		>
+		<div class="flex flex-row items-center text-sm h-14 w-full bg-blue-300 p-5 opacity-90">
 			<img src="~/assets/images/logo.webp" alt="log" class="size-11" />
-			<h1
-				class="text-3xl font-bold font-mono text-stroke-yellow w-full text-start pl-6"
-			>
+			<h1 class="text-3xl font-bold font-mono text-stroke-yellow w-full text-start pl-6">
 				Movies World
 			</h1>
-			<div
-				v-if="weatherData && geolocationData"
-				class="flex flex-row items-center justify-end w-full pr-3"
-			>
-				<img
-					:src="weatherData.current.condition.icon"
-					:alt="weatherData.current.condition.text"
-					class="size-10"
-				/>
+			<div v-if="weatherData && geolocationData" class="flex flex-row items-center justify-end w-full pr-3">
+				<img :src="weatherData.current.condition.icon" :alt="weatherData.current.condition.text"
+					class="size-10" />
 				{{ weatherData.current.temp_c }}°C {{ geolocationData.city }} ,
 				{{ geolocationData.country }}
 				<p v-if="!weatherData && !errorMessage">searching weather data...</p>
@@ -56,14 +46,12 @@ const handleLogout = () => {
 					{{ errorMessage || weatherErrorMessage }}
 				</p>
 			</div>
-			<button
-				@click="handleLogout"
-				class="rounded-xl w-20 p-2 boder font-bold bg-blue-100"
-			>
+			<button @click="handleLogout" class="rounded-xl w-20 px-2 boder font-bold bg-blue-100">
 				Logout
 			</button>
 		</div>
-		<div class="flex flex-row bg-blue-200 h-10 items-center text-sm opacity-90">
+		<div class="flex flex-row justify-between bg-blue-200 h-10 px-2 items-center text-sm opacity-90">
+			<nuxt-link to="/main" class="p-3 hover:underline"> Home </nuxt-link>
 			<nuxt-link to="/movies/searchMovies" class="p-3 hover:underline">
 				Search Info
 			</nuxt-link>
@@ -74,6 +62,7 @@ const handleLogout = () => {
 			<nuxt-link to="/billboard/mainbillboard" class="p-3 hover:underline">
 				Billboard
 			</nuxt-link>
+			<nuxt-link to="/editProfile" class="p-3 hover:underline">Edit Profile</nuxt-link>
 		</div>
 	</div>
 </template>
