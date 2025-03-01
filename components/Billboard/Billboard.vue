c0b65a590046ae945acf936ed690657ed1b5f588
David <156122937+XefDavid@users.noreply.github.com>
2025-03-01 09:23:34 +0100

    style: fix desing in mapping data and create button link to trailer

--- components/Billboard/Billboard.vue
+++ components/Billboard/Billboard.vue
@@ -10,23 +10,54 @@ export default {
 		return { movies, error };
 	},
 };
+
 </script>
 <template>
-	<div class="flex flex-col items-center justify-start pt-10 h-[100vh] overflow-auto">
-		<div v-if="movies.length" class="">
-			<h1 class="text-5xl font-bold font-mono text-stroke-yellow w-full p-8 text-center">Showing now !!</h1>
+	<div class="flex flex-col items-center justify-start pt-10 h-[100vh] w-full overflow-auto">
+		<div v-if="movies.length" class="w-full p-20">
+			<h1 class="text-5xl font-bold font-mono text-stroke-yellow pb-10 w-full text-center">Showing now !!</h1>
 
 			<div class="">
-				<ul class="gap-20 justify-center flex flex-col   w-[70%]">
+				<ul class="gap-20 justify-center flex flex-col w-full">
 					<li v-for="movie in movies" :key="movie.film_id"
-						class="border border-black flex flex-col w-[340px] h-[550px] bg-blue-100">
-						<h3 class="text-center text-normal text-gray-700 font-mono font-extrabold w-[250px]">
+						class="border border-black flex flex-col w-full  bg-blue-100">
+						<h3
+							class="text-center text-normal text-gray-700 font-mono font-extrabold w-full text-3xl underline p-4">
 							{{ movie.film_name }}
 						</h3>
 
-						<img :src="movie.images.poster[1]?.medium.film_image || '/placeholder.jpg'
-							" :alt="movie.film_name" class="border border-black w-[250px] h-[400px] p-4" />
+						<div class="flex flex-row gap-4 p-4">
+							<img :src="movie.images.poster[1]?.medium.film_image || '/placeholder.jpg'
+								" :alt="movie.film_name" class="border border-black w-[250px] h-[400px] p-4" />
+							<div class="flex flex-col  justify-start items-start p-2 gap-2">
+								<p v-if="movie.release_dates.length"
+									class="text-center text-gray-700 font-mono font-bold w-[250px] flex flex-row">
+									Fecha de estreno:
+								<p class="text-base pl-3">{{ movie.release_dates[0]?.release_date }}</p>
+								</p>
+								<p v-if="movie.synopsis_long"
+									class="text-start text-normal text-gray-700 font-mono  w-full">
+									{{ movie.synopsis_long }}
+								</p>
+								<p v-else class="text-start text-normal text-gray-700 font-mono  w-full">Synopsis no
+									available</p>
 
+								<div class="mt-auto ml-auto p-5">
+									<a v-if="movie.film_trailer" :href="movie.film_trailer" target="_blank"
+										rel="noopener noreferrer"
+										class="px-4 py-2 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-700 transition h-[36px] w-full">
+										See Trailer 🎬
+									</a>
+									<a v-else
+										class="px-4 py-2 bg-gray-500 text-white font-bold rounded-lg h-[36px] w-full">Trailer
+										not available</a>
+								</div>
+
+							</div>
+
+
+
+						</div>
 
 					</li>
 				</ul>
@@ -39,6 +70,8 @@ export default {
 			<p>Cargando películas...</p>
 		</div>
 	</div>
+
+
 </template>
 
 <style scoped>
